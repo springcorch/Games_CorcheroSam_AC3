81ca7aee2c31820bfb4b1ef877a72535cc334e30
Sam <sam.corchero@estudiant.enti.cat>
2025-10-13 18:28:58 +0200

    MainMenu Template commit
    - A title
    - A puntuation that is a text plus a variable that starts by 0.
    - A button that OnClick it changes to the TickTackToe screen TODO//

--- app/src/main/java/com/example/videogame/MainActivity.kt
+++ app/src/main/java/com/example/videogame/MainActivity.kt
@@ -6,11 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.compose.AppTheme
+import com.example.videogame.ui.theme.screens.MainMenu
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,27 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    OnScreen()
+                    MainMenu(modifier = Modifier)
                 }
             }
         }
-    }
-}
-
-//TODO: GAMEPLAY AND MAIN MENU//
-@Composable
-fun OnScreen(){
-    Text(
-        text = "Prueba",
-        color = MaterialTheme.colorScheme.primary,
-    )
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun OnScreenPreview() {
-    MaterialTheme {
-        OnScreen()
     }
 }
--- app/src/main/java/com/example/videogame/ui/theme/screens/MainMenu.kt
+++ app/src/main/java/com/example/videogame/ui/theme/screens/MainMenu.kt
@@ -1,8 +1,50 @@
 package com.example.videogame.ui.theme.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+// Screen MainMenu
 @Composable
-fun MainMenu(){
-
+fun MainMenu(modifier: Modifier){
+    // Variable that counts the total points the player has
+    val points: Int = 0;
+    Column(modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        Text("PLAY & EARN",
+            fontSize = 64.sp,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Center)
+        Text("Puntuation: $points",
+            fontSize = 32.sp,
+            modifier = Modifier.padding(16.dp))
+        //On Click - New screen TickTackToe
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth().size(128.dp)
+                                .padding(top = 16.dp, bottom = 16.dp),
+            shape = RectangleShape) {
+            Text("START",
+                fontSize = 52.sp)
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun MainMenuPreview() {
+    MainMenu(modifier = Modifier)
 }
