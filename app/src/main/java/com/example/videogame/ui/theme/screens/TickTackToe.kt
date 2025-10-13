da24209d4ad380d1abb22732331f95dac636f3ce
Sam <sam.corchero@estudiant.enti.cat>
2025-10-13 20:01:17 +0200

    TTT Header commit
    - Confirmation that header works good.

--- app/src/main/java/com/example/videogame/ui/theme/screens/TickTackToe.kt
+++ app/src/main/java/com/example/videogame/ui/theme/screens/TickTackToe.kt
@@ -1,8 +1,34 @@
 package com.example.videogame.ui.theme.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.videogame.ui.theme.components.Header
 
 @Composable
 fun TickTackToe(){
+    Column(modifier = Modifier.fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background),
+    horizontalAlignment = Alignment.CenterHorizontally) {
+        Header(0)
+        Text("TICK TACK TOE",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onBackground)
 
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TickTackToePreview() {
+    TickTackToe()
 }
