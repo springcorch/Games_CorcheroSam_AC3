477b8941455afa79e083d9810162c9280de3e3b4
springcorch <sam.corchero@estudiant.enti.cat>
2025-10-19 01:16:53 +0200

    CX Gameplay Template commit

--- app/src/main/java/com/example/videogame/MainActivity.kt
+++ app/src/main/java/com/example/videogame/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.compose.AppTheme
+import com.example.videogame.ui.theme.screens.CrashX
 import com.example.videogame.ui.theme.screens.MainMenu
 import com.example.videogame.ui.theme.screens.TickTackToe
 
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TickTackToe()
+                    CrashX()
                 }
             }
         }
